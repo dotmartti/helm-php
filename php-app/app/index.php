52d436c5f603a763100b3930030e98becddfebed
Martti Remmelgas <martti.remmelgas@gmail.com>
2021-05-02 01:58:45 +0300

    typo in index.php

--- php-app/app/index.php
+++ php-app/app/index.php
@@ -2,6 +2,6 @@
 if (isset($_GET['n'])) {
     echo $_GET['n']*$_GET['n'];
 } else {
-    echo "please set a variable n to a number you want to square"
+    echo "please set a variable n to a number you want to square";
 }
 ?>
