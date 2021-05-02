58a8dcb1ab2acc10a6422ab1f6c4039ad35df33a
Martti Remmelgas <martti.remmelgas@gmail.com>
2021-05-02 23:10:26 +0300

    switch on the Postgres support in PHP

--- php-app/app/blacklisted.php
+++ php-app/app/blacklisted.php
@@ -1,6 +1,14 @@
 <?php
-  print "Recording visiting IP ".$_SERVER["REMOTE_ADDR"]." in the Database ".$_SERVER["PGDATABASE"]."\n\n";
-
   # this page returns HTTP 444 to users
   http_response_code(444);
+
+  $connection = pg_connect ("host=".$_SERVER["PGHOST"]." dbname=".$_SERVER["PGDATABASE"]." user=".$_SERVER["PGUSER"]." password=".$_SERVER["PGPASSWORD"]);
+  if($connection) {
+    print "connected\n";
+  } else {
+    print "there has been an error connecting\n";
+  } 
+  print "Recording visiting IP ".$_SERVER["REMOTE_ADDR"]." in the Database ".$_SERVER["PGDATABASE"]."\n\n";
+
+  
 ?>
