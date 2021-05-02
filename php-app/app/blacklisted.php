1a8887669037015f0d5c34acc216f8b0bf696779
Martti Remmelgas <martti.remmelgas@gmail.com>
2021-05-03 01:06:53 +0300

    blacklisted endpoint blocks an incoming IP and inserts it into a database table called blocklist

--- php-app/app/blacklisted.php
+++ php-app/app/blacklisted.php
@@ -3,12 +3,37 @@
   http_response_code(444);
 
   $connection = pg_connect ("host=".$_SERVER["PGHOST"]." dbname=".$_SERVER["PGDATABASE"]." user=".$_SERVER["PGUSER"]." password=".$_SERVER["PGPASSWORD"]);
-  if($connection) {
-    print "connected\n";
-  } else {
-    print "there has been an error connecting\n";
+  if(!$connection) {
+    # DEBUG OUTPUT print "ERROR: Connecting the database\n";
+    exit;
   } 
-  print "Recording visiting IP ".$_SERVER["REMOTE_ADDR"]." in the Database ".$_SERVER["PGDATABASE"]."\n\n";
 
+  # create a table to record blocking
+  $create = "CREATE TABLE IF NOT EXISTS blocklist (ip varchar(39) PRIMARY KEY";
+  $create .= ",path varchar(255), timestamp TIMESTAMP NOT NULL DEFAULT NOW())";
+  $result = pg_query($connection, $create);
+  if (!$result) {
+    # DEBUG OUTPUT echo "ERROR: Couldn't create the blocklist table.\n";
+    exit;
+  }
   
+  # if IP is blocked, then stop all processing
+  $path = $_SERVER["REQUEST_URI"];
+  $ipaddress = $_SERVER["REMOTE_ADDR"];
+  $select="SELECT count(1) ip FROM blocklist WHERE ip='".$ipaddress."'";
+
+  $result2 = pg_query($connection, $select);
+  # DEBUG OUTPUT echo "query result ".pg_fetch_assoc($result2, 0)['ip']."\n";
+  if (pg_fetch_assoc($result2, 0)['ip'] == "1") {
+    # DEBUG OUTPUT echo "IP blocked: $ipaddress.\n";
+    exit;
+  }
+
+  # insert the ipaddress to be blocked
+  $insert="INSERT INTO blocklist VALUES ('".$ipaddress."', '".$path."')";
+  $result3 = pg_query($connection, $insert);
+  if (!$result3) {
+    # DEBUG OUTPUT echo "ERROR Couldn't block the IP $ipaddress.\n";
+    exit;
+  }
 ?>
--- php-app/app/index.php
+++ php-app/app/index.php
@@ -1,6 +1,3 @@
-<html>
- <body>
-  <h1>
 <?php
 if (isset($_GET['n'])) {
     echo $_GET['n']*$_GET['n'];
@@ -8,12 +5,3 @@ if (isset($_GET['n'])) {
     echo "please set a variable n to a number you want to square";
 }
 ?>
-  </h1>
-  <p>
-<?php
-    print "Your IP address is ".$_SERVER['REMOTE_ADDR']."\n\n";
-    print "Database ".$_SERVER["PGDATABASE"]." at ".$_SERVER["PGHOST"]."\n";
-?>
-  </p>
- </body>
-</html>
