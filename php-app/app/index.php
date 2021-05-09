d885e6c0b19fd93f230ec4701a4a477097af5e35
Martti Remmelgas <martti.remmelgas@gmail.com>
2021-05-09 14:33:47 +0300

    a little change in app in case we have no IPs blocked yet

--- php-app/app/index.php
+++ php-app/app/index.php
@@ -2,6 +2,7 @@
   # 
   $connection = pg_connect ("host=".$_SERVER["PGHOSTREAD"]." dbname=".$_SERVER["PGDATABASE"]." user=".$_SERVER["PGUSER"]." password=".$_SERVER["PGPASSWORD"]);
   if(!$connection) {
+    # let's not continue when we cannot verify the IP from the database
     exit;
   }
 
@@ -11,15 +12,19 @@
 
   # if IP is blocked, then stop all processing
   $result2 = pg_query($connection, $select);
-  if (pg_fetch_assoc($result2, 0)['ip'] == "1") {
-    http_response_code(403);
-    echo "Your IP is blocked!";
-    exit;
+
+  # only deal with query, when we actually have some IPs blocked (table exists)
+  if ($result2) {
+    if (pg_fetch_assoc($result2, 0)['ip'] == "1") {
+      http_response_code(403);
+      echo "Your IP is blocked!";
+      exit;
+    }
   }
 
-if (isset($_GET['n'])) {
-    echo $_GET['n']*$_GET['n'];
-} else {
-    echo "please set a variable n to a number you want to square";
-}
+  if (isset($_GET['n'])) {
+      echo $_GET['n']*$_GET['n'];
+  } else {
+      echo "please set a variable n to a number you want to square";
+  }
 ?>
